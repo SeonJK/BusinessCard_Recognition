3c675bc488446d2ab83b95f398b5754984753bae
tjswhdrbs14 <tjswhdrbs14@naver.com>
2019-06-27 16:05:44 +0900

    OpenCV Example 자잘한 수정

--- OcvExample2/app/src/main/java/com/example/ocvexample2/MainActivity.java
+++ OcvExample2/app/src/main/java/com/example/ocvexample2/MainActivity.java
@@ -74,7 +74,7 @@ public class MainActivity extends AppCompatActivity
             }
         }
 
-        mOpenCvCameraView = (CameraBridgeViewBase)findViewById(R.id.activity_surface_view);
+        mOpenCvCameraView = findViewById(R.id.activity_surface_view);
         mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
         mOpenCvCameraView.setCvCameraViewListener(this);
         mOpenCvCameraView.setCameraIndex(0); // front-camera(1),  back-camera(0)
